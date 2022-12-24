17dcebbc5433458e2f5d0f6c9088c9985fe70d19
Dominik Bedenic <60035381+DBDoco@users.noreply.github.com>
2022-12-24 18:06:57 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,45 @@
+
+<h1 align="center">
+  Three.js - Phone Showcase
+  <br>
+</h1>
+
+<h4 align="center"><a href="https://threejs.org" target="_blank">Three.js</a> 3D phone that can showcase feature website or app. Made for prototyping. Deployed with <a href="https://vitejs.dev" target="_blank">Vite.js</a>.<br>Animations were made using <a href="https://greensock.com/gsap/">GSAP</a>. Phone model is made in <a href="https://www.blender.org"> Blender</a> (.blend file is also included in repo).</h4>
+
+<p align="center">
+  <img src="https://media0.giphy.com/media/P1MKKlweLBBtlmarrp/giphy.gif?cid=790b76110f996ba10bbf380f9a17d3a9292272044a323bc6&rid=giphy.gif&ct=g" alt="phone" />
+</p>
+
+
+
+
+
+## How To Use
+
+Clone this repository
+```bash
+$ git clone https://github.com/DBDoco/Three.js-Phone-Showcase.git
+```
+Go into the repository
+```bash
+$ cd Three.js-Phone-Showcase
+```
+Run development build
+```bash
+$ npm run dev
+```
+
+or
+
+Build the project
+```bash
+$ npm run build
+```
+
+## Try it live
+<a href="">Here</a>.
+
+
+## Troubleshooting
+
+For running dev build or building the project you will need <a href="https://nodejs.org/en/">Node.js</a>.
---
+++ main.js
@@ -0,0 +1,89 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { Triangle } from 'three'
+import gsap from 'gsap'
+
+const gltfLoader = new GLTFLoader()
+let tl = new gsap.timeline()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+//Phone
+
+gltfLoader.load('phone.gltf', (gltf)=>{
+    gltf.scene.scale.set(0.5, 0.5, 0.5),
+    gltf.scene.rotation.set(0, 3.3, 0),
+    scene.add(gltf.scene)
+
+    tl.to(gltf.scene.rotation, {y:4.7, duration: 1})
+    tl.to(gltf.scene.scale, {x:0.3, y:0.3, z:0.3, duration: 1}, "-=1")
+    tl.to(gltf.scene.scale, {x:0.35,y:0.35,z:0.35, duration:1}, "-=1")
+    tl.fromTo('.heading', {opacity: 0}, {opacity:1})
+    tl.fromTo('nav', {y:"-100%"}, {y:"0%"})
+
+})
+
+// Lights
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 1)
+ambientLight.position.x = 2
+ambientLight.position.y = 3
+ambientLight.position.z = 4
+scene.add(ambientLight)
+
+
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 0
+camera.position.y = 0
+camera.position.z = 2
+scene.add(camera)
+
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    alpha:true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+
+
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+
+    const elapsedTime = clock.getElapsedTime()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
